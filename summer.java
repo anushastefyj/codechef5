8a618e5934f4460884b4d0282078df5dd3aa1e5a
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:06:55 +0530

    Add files via upload

---
+++ summer.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class summer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a>35)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
