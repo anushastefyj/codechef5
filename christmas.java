0755ee2df630bdd1da77590ab3fcdc176ae1c492
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:03:57 +0530

    Add files via upload

---
+++ christmas.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class christmas
+{
+public static void main(String args[]) throws java.lang.Exception
+{
+Scanner Sc=new Scanner(System.in);
+int a=25;
+if(a==25)
+{
+System.out.print("CHRISTMAS");
+}
+else
+{
+System.out.println("ORDINARY");
+}
+    
+   }
+}
