e121d37dc0207210982580d7bc0bb3b5e15fff30
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:08:54 +0530

    Add files via upload

---
+++ papercutting.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class papercutting
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner Sc=new Scanner(System.in);
+		int t=Sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int a=Sc.nextInt();
+		    int b=Sc.nextInt();
+		    int c=((a/b)*(a/b));
+		    System.out.println(c);
+		    
+		}
+
+	}
+}
