22af199e3ad8d77c399d35cdaecae9c42533ce26
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:32:06 +0530

    Add files via upload

---
+++ healthysleep.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class healthysleep
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int H=sc.nextInt();
+		if(H<8)
+		{
+		   System.out.println("LESS");
+        }
+		   else if(H==8)
+		{
+		    System.out.println("PERFECT");
+		}
+		else
+        {
+		    System.out.println("MORE");
+		}
+
+	}
+}
