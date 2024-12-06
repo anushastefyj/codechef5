aabb31cbccaa4b9eaf9ad647eb793f76f8a050eb
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:05:33 +0530

    Add files via upload

---
+++ parole.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class parole
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner Sc=new Scanner(System.in);
+		int a=Sc.nextInt();
+		if(a>=7)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+	}
+}
