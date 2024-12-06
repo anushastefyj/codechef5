8668ec84a272d34d04b6d5c1d1477ff518ce0d73
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:10:25 +0530

    Add files via upload

---
+++ idesofmarch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class idesofmarch
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner Sc=new Scanner(System.in);
+		int a=Sc.nextInt();
+		if(a==15)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+	}
+}
