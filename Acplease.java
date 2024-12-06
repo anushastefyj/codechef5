e7e6b51924552f720304d0338d194bf253a7c9c1
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 11:21:00 +0530

    Add files via upload

---
+++ Acplease.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Acplease
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		if(T>30)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+		
+
+	}
+}
