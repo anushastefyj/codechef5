c1cc97a8e4bcf77a9fbcb0d0ad9c04b584ea954c
anushastefyj <24hr5a3001@mtieat.org>
2024-12-06 10:57:56 +0530

    Add files via upload

---
+++ aditiya.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class aditiya
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner Sc=new Scanner(System.in);
+		int a=Sc.nextInt();
+		if(a>=5 && a<=18)
+		{
+		    System.out.println("aditiya");
+		}else{
+		    System.out.println("waiting for aditiya");
+		}
+	}
+}
